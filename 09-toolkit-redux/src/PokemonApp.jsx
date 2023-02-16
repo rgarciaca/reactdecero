58daa800c639d03a01fb5133b4828e235eeb6358
Roberto Garcia <rgarciaca@outlook.com>
2023-02-16 19:35:28 +0100

    Ejemplo 09

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getPokemons } from "./store/slices/pokemon";
+
+export const PokemonApp = () => {
+
+    const { page, pokemons = [], isLoading} = useSelector(state => state.pokemons );
+    const dispatch = useDispatch();
+    useEffect( () => {
+        dispatch( getPokemons());
+    }, [])
+
+  return (
+    <>
+        <h1>PokemonApp</h1>
+        <hr />
+        <span>Loading: { isLoading ? 'True' : 'False' }</span>
+
+        <ul>
+          {           
+            pokemons.map( (  pokemon  ) => (
+              <li key={pokemon.name}>{ pokemon.name }</li>
+            ))
+          }
+
+        </ul>
+
+        <button disabled={ isLoading } onClick={ () => dispatch( getPokemons(page) )} >Next Page</button>
+
+    </>
+  )
+}
---
+++ 09-toolkit-redux/src/store/api/todosApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const todosApi = createApi({
+  reducerPath: 'todos',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => `/todos`,
+    }),
+    getTodoById: builder.query({
+        query: ( id ) => `/todos/${ id }`
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetTodosQuery, useGetTodoByIdQuery } = todosApi;
