87e81f50e2eb264fc8d454951710a8ecde8bf51d
Roberto Garcia <rgarciaca@outlook.com>
2023-02-20 23:01:22 +0100

    Ejemplo 10

---
+++ 10-calendar/src/calendar/components/FabAddNew.jsx
@@ -0,0 +1,30 @@
+import { addHours } from "date-fns";
+import { useCalendarStore, useUiStore } from "../../hooks"
+
+export const FabAddNew = () => {
+
+     const { openDateModal } = useUiStore();
+     const { setActiveEvent } = useCalendarStore();
+
+     const handleClickNew = () => {
+        setActiveEvent({
+            title: '',
+            notes: '',
+            start: new Date(),
+            end: addHours(new Date(), 1 ),
+            bgColor: '#fafafa',
+            user: {
+                _id: '123',
+                name: 'Roberto'
+            }
+        });
+
+        openDateModal();
+     }
+
+  return (
+    <button className="btn btn-primary fab" onClick={ handleClickNew }>
+        <i className="fas fa-plus" ></i>
+    </button>
+  )
+}
---
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { getTime } from "date-fns";
+
+
+export const useCalendarStore = () => {
+  
+    const dispatch = useDispatch();
+    const { events, activeEvent } = useSelector( state => state.calendar );
+
+    const setActiveEvent = ( calendarEvent ) => {
+        dispatch( onSetActiveEvent( calendarEvent ) );
+    }
+
+    const startSavingEvent = async ( calendarEvent ) => {
+
+        if ( calendarEvent._id ) {
+            dispatch( onUpdateEvent( { ...calendarEvent } ) );
+        } else {
+            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+        }
+    }
+
+    const startDeletingEvent = () => {
+        dispatch( onDeleteEvent() );
+    }
+
+    return {
+        //* Propiedades
+        events,
+        activeEvent,
+        hasEventSelected: !!activeEvent,
+
+        //* Métodos
+        setActiveEvent,
+        startSavingEvent,
+        startDeletingEvent,
+    }
+}
